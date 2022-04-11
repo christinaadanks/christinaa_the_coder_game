c895226787b70c239ba4ed9a5021d2cfd7ee483c
christinaadanks <christinaa.danks@me.com>
2022-04-11 15:22:53 -0700

    Moved Tilset tsx files out of the pycharm file (not needed, only needed in Tile)

--- levels/tilesets/playerstart_end.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="playerstart_end" tilewidth="32" tileheight="32" tilecount="8" columns="4">
- <image source="../../graphics/characters/player/playerstart_end.png" width="128" height="64"/>
-</tileset>
